cecf02588bf14cf089486bb0a7be07ec3226f2ce
Roman-26 <roman26@i.ua>
2020-02-24 11:28:43 +0200

    Commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { ContactiComponent } from './components/contacti/contacti.component';
+
+const appRoutes: Routes = [
+  {path: '', component: CarComponent},
+  {path: 'about', component: ContactiComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    ContactiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -6,10 +6,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car.component.css']
 })
 export class CarComponent implements OnInit {
+  name: string;
+  speed: number;
+  model: string;
+  colors: Colors;
+  options: string[];
+  isEdit: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.name = 'Audi';
+    this.speed = 234;
+    this.model = 'RS 8';
+    this.colors = {
+      car: 'Білий',
+      salon: 'Чорний',
+      wheels: 'Сірий'
+    };
+    this.options = ["ABC","AutoPilot","Паркинг"];
   }
 
+  showEdit(){
+    this.isEdit = !this.isEdit;
+  }
+
+  addOpt(option){
+    this.options.unshift(option);
+    return false;
+  }
+
+  deleteOpt(option){
+    for(let i=0;i<this.options.length;i++){
+      if(this.options[i]==option){
+        this.options.splice(i,1);
+        break;
+      }
+    }
+  }
+
+ carSelect(carName){
+    if(carName=='bmv'){
+      this.name = 'BMV';
+      this.speed = 300;
+      this.model = 'M5';
+      this.colors = {
+        car:'Зелений',
+        salon:'Білий',
+        wheels:'Чорний'
+      };
+      this.options = ["ABC","Круїз контроль","Паркинг",'Кондиціонер'];
+    }else if (carName=='audi'){
+      this.name = 'Audi';
+      this.speed = 234;
+      this.model = 'RS 8';
+      this.colors = {
+        car:'Білий',
+        salon:'Чорний',
+        wheels:'Сірий'
+      };
+      this.options = ["ABC","AutoPilot","Паркинг"];
+    }else {
+      this.name = 'Мерседес';
+      this.speed = 180;
+      this.model = 'Е230';
+      this.colors = {
+        car:'Білий',
+        salon:'Білий',
+        wheels:'Сірий'
+      };
+      this.options = ["Система трека","AutoPilot","Паркинг",'ABC','Клімат контроль'];
+    }
+ }
+}
+
+interface Colors {
+  car:string,
+  salon:string,
+  wheels:string
 }
